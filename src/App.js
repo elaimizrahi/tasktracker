a6a79b33fe843f6b13a20690f9b1796b1ff49d09
elaimizrahi <mizrahi.elai@gmail.com>
2022-12-21 00:46:23 -0500

    hello

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 import { useState } from 'react'
 
 function App() {
+
+  const [showAddTask, setShowAddTask] = useState(true)
+
   const [tasks,setState] = useState([
     {
         id:1,
@@ -23,14 +27,28 @@ function App() {
         reminder:true,
     }
 ])
+
+const addTask = (task) =>{
+  const id = Math.floor(Math.random() * 10000) + 1
+
+  const newTask = { id, ...task}
+  setState([ ...tasks, newTask])
+}
+
 //Delete Task
   const deleteTask = (id) => {
     setState(tasks.filter((task)=> task.id !== id))
   }
+
+  const toggleReminder = (id) =>{
+    setState(tasks.map((task)=>task.id === id ? { ...task, reminder: !task.reminder} : task))
+  }
+
   return (
     <div className="container">
-      <Header />
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}/> : 'No Tasks'}
+      <Header onAdd={()=>setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask}/>}
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks'}
     </div>
   );
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,6 @@
 import React from 'react'
 
-const Button = ({color, text}) => {
-    const onClick = () => {
-        console.log('Click')
-    }
+const Button = ({color, text, onClick}) => {
   return (        
   <button onClick={onClick} style = {{backgroundColor: color}}className = 'btn'>
     {text}
